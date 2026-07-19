2327474f0341f5c5f4427911c2d3d5bd2c005354
mohamedalswaify <mohamedgamal1455@gmail.com>
2026-07-19 21:51:18 +0300

    19-7-2026

---
+++ 2_CSharp/ThirdConsoleApp/ThirdConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdConsoleApp
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+
+        //    //Type Errors
+
+        //    // 1.Syntax Error
+        //    Console.WriteLine("Hello World!");
+
+        //    //2.Runtime Error
+        //    //var x = 10;
+        //    //var y = 0;
+        //    //Console.WriteLine(x/y);
+
+        //    //3.Logical Error
+        //    Console.WriteLine(13 + 7 + 10 / 3);
+
+        //    Console.ReadKey(true);
+        //}
+    }
+}
