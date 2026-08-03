9fdf4a6096e4e47feca184c880c38d0d07b098ca
mohamedalswaify <mohamedgamal1455@gmail.com>
2026-08-03 22:16:04 +0300

    3-8-2026

---
+++ 2_CSharp/ThirdConsoleApp/ThirdConsoleApp/OOP_Level3/day4/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdConsoleApp.OOP_Level3.day4
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        private double _salary;
+        public double Salary {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if(value>=0)
+                _salary = value;
+                else
+                    Console.WriteLine("Salary cannot be negative.");
+            }
+        }
+
+        public Employee(string name, int age, double salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+
+        public virtual string DisplayEmployeeDetails()
+        {
+            return $" Welcome Name: {Name}, Age: {Age}, Salary: {Salary}";
+        }
+
+        public abstract double CalculateAnnualSalary();
+     
+
+    }
+}
--- 2_CSharp/ThirdConsoleApp/ThirdConsoleApp/OOP_Level3/day4/EmployeeTest.cs
+++ 2_CSharp/ThirdConsoleApp/ThirdConsoleApp/OOP_Level3/day4/EmployeeTest.cs
@@ -4,23 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ThirdConsoleApp.OOP_Level3.day3
+namespace ThirdConsoleApp.OOP_Level3.day4
 {
     internal class EmployeeTest
     {
-        static void Mainn()
+        static void Main()
         {
-            Employee e1 = new Employee("John", 30, 5000);
+          //  Employee e1 = new Employee("John", 30, 1000);
             EmployeeBase e2 = new EmployeeBase("Ali", 28, 1000, 500);
             EmployeeHour e3 = new EmployeeHour("Sara", 25, 1000, 10,60);
 
 
 
-            Console.WriteLine($"{e1.DisplayEmployeeDetails()} Annual Salary: {e1.CalculateAnnualSalary()} SR");
+
+            //Console.WriteLine($"{e1.DisplayEmployeeDetails()} Annual Salary: {e1.CalculateAnnualSalary()} SR");
             Console.WriteLine($"{e2.DisplayEmployeeDetails()} Annual Salary: {e2.CalculateAnnualSalary()} SR");
             Console.WriteLine($"{e3.DisplayEmployeeDetails()} Annual Salary: {e3.CalculateAnnualSalary()} SR");
 
          
+
             Console.ReadKey();
         }
     }
