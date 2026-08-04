b413854ff62f3f2aed3db315b89b3c1b28b8a13d
mohamedalswaify <mohamedgamal1455@gmail.com>
2026-08-04 20:17:43 +0300

    4-8-2026

---
+++ 2_CSharp/ThirdConsoleApp/ThirdConsoleApp/OOP_Level3/day5/accounts/Account_Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdConsoleApp.OOP_Level3.day5.accounts
+{
+    internal class Account_Test
+    {
+        static void Main()
+        {
+            //BankAccount b1 = new BankAccount("8347467", "Ahmed", 2000);
+            SavingsAccount b2 = new SavingsAccount("87654333", "Mohammed", 8000, 2);
+            CurrentAccount b3 = new CurrentAccount("4563728", "Khalied", 9000, 5000);
+
+            //Console.WriteLine($"{b1.DisplayAccount()} Deposit: {b1.Deposit(2000)}");
+            Console.WriteLine($"{b2.DisplayAccount()} Total Rate: {b2.CalculateInterest()}");
+            Console.WriteLine($" {b3.DisplayAccount()} With Draw: {b3.Withdraw(3000)}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2_CSharp/ThirdConsoleApp/ThirdConsoleApp/OOP_Level3/day5/accounts/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdConsoleApp.OOP_Level3.day5.accounts
+{
+    public abstract class BankAccount
+    {
+        public string AccountNumber;
+        public string OwnerName;
+        private double _Balance;
+
+        public double Balance
+        {
+            get
+            {
+                return _Balance;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _Balance = value;
+                }
+                Console.WriteLine("Invalied balance");
+            }
+        }
+        public BankAccount(string account_number, string owner_name, double balance)
+        {
+            AccountNumber = account_number;
+            OwnerName = owner_name;
+            Balance = balance;
+        }
+        public abstract string DisplayAccount();
+
+        public double Deposit(double amount)
+        {
+            Balance += amount;
+            return Balance; ;
+        }
+    }
+}
